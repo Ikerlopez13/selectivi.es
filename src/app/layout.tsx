a5267073190797fb18e12cb6bc1080581ac5d349
Iker.dev <lopezalegreiker@gmail.com>
2025-09-21 11:58:58 +0200

    feat(analytics): add @vercel/analytics and inject <Analytics /> in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: 'SelectiviES',
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
